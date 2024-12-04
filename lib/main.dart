0897145870a3d566b2e73a36536212916886e165
isantos <hancellsud@gmail.com>
2024-12-04 10:29:10 -0500

    Israel Santos

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:evaluacion_parcial_01/pantalla1.dart';
 import 'package:evaluacion_parcial_01/pantalla2.dart';
 
-
-void main(){
-  runApp(Pantalla0());
-
+void main() {
+  runApp(const MyApp());
 }
 
-class Pantalla0 extends StatelessWidget{
-  const Pantalla0({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-   Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Evaluación Parcial 01',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const Pantalla0(), // Pantalla inicial
+    );
+  }
+}
+
+class Pantalla0 extends StatelessWidget {
+  const Pantalla0({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Evaluacion Parcial 01", style: TextStyle(color: Colors.black,),),
+        title: const Text(
+          "Evaluación Parcial 01",
+          style: TextStyle(color: Colors.black),
+        ),
         backgroundColor: const Color.fromARGB(255, 105, 206, 114),
       ),
-      body: Column(
-        children: [
-          Text("Evaluacion Parcial 01", style: TextStyle(color: Colors.black,),),
-          pagina_btn(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Pantalla Principal",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            
+          ],
+        ),
       ),
     );
-
-   }
   }
+}
+
 
 //   ///////////////crearemos accesos para las demas pantallas/////////////////
 
